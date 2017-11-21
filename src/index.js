5cb0809e8254bebed95b4aca85ac35179f2914dd
noah <noah@iamnoah.com>
2017-11-21 00:13:57 -0800

    load images

--- src/index.js
+++ src/index.js
@@ -1,6 +1,23 @@
 import Flickr from 'Flickr';
 import utils from 'utils';
 
+
+const CONTAINER = document.getElementById('container');
+
+function img(src) {
+  const element = document.createElement('img');
+  element.src = src;
+  return element;
+}
+
+function addImage(img) {
+  CONTAINER.appendChild(img);
+}
+
 Flickr.onLoad(photos => {
   utils.info('photos', photos);
+
+  photos.forEach(photo => {
+    addImage(img(photo.media.m));
+  });
 });
